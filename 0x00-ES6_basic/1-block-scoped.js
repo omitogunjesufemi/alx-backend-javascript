5c8acdb10e13e247de5eb26006f726a6a04b3337
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-09 02:37:19 +0100

    Added the eslint disabling comment for no-unused var

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
   }
 
   return [task, task2];
