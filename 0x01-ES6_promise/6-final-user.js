90c52b6ea4f61a51c62c39d694e5f1f0a657fd15
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-11 03:09:53 +0100

    Promise.allsettled for handling multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const userSignUp = signUpUser(firstName, lastName);
+  const userPhotoUpload = uploadPhoto(filename);
+
+  return Promise.allSettled([userSignUp, userPhotoUpload]).then((response) => response);
+}
