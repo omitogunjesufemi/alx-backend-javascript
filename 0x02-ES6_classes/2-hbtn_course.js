3c5cb89cd02b069d0461ee1c7ecabbd6c847e8da
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-12 02:09:47 +0100

    Corrected eslint errors

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,16 +2,12 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
-    } else {
-      this._name = name;
-    }
-
-    if (typeof length !== 'number') {
+    } else if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
-    } else {
-      this._length = length;
     }
 
+    this._name = name;
+    this._length = length;
     this._students = students;
   }
 
@@ -42,8 +38,6 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (value) {
-      this._students = value;
-    }
+    this._students = value;
   }
 }
