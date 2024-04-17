75b64a797ec41a8c1a3f255b806d9cd726d65360
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-17 14:23:00 +0100

    Inserting a value in a typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = ArrayBuffer(length);
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
 
-  const int8View = Int8Array(buffer);
+  const buffer = new ArrayBuffer(length);
+  const int8View = new DataView(buffer);
+  int8View.setInt8(position, value);
+
+  return (int8View);
 }
