63950c4d0efb691d8b1eac15fc40cf4d53318d4f
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-19 14:23:02 +0100

    Using weakMap to track a function call invocation

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,16 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (weakMap.has(endpoint) !== true) {
+    weakMap.set(endpoint, 1);
+  } else {
+    weakMap.set(endpoint,
+      weakMap.get(endpoint) + 1);
+
+    if (weakMap.get(endpoint) >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+}
+
+export { weakMap, queryAPI };
