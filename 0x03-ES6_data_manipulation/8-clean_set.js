31acca804f341aa7d30a19663160330910cfbb0c
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-19 12:52:33 +0100

    Iterate through a Set and concatenate the string based on what it starts with

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,13 @@
 export default function cleanSet(set, startString) {
   const resultList = [];
 
-  if (set) {
-    for (const elem of set) {
-      if (elem.startsWith(startString) && startString !== '') {
-        resultList.push(elem.slice(3));
-      }
+  if (typeof startString !== 'string') {
+    return ('');
+  }
+
+  for (const elem of set) {
+    if (elem.startsWith(startString) && startString !== '') {
+      resultList.push(elem.slice(3));
     }
   }
 
