545f6de1b1cf618895a6c7283b3af376b63b2780
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-08-01 17:06:48 +0100

    A small HTTP server using express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const port = 1245;
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(port);
